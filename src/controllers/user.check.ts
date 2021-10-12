9808862be24d143d4b37ab45556b3a1980778254
Jongin Oh <ohjongin@gmail.com>
2021-10-12 22:36:34 +0900

    CHKN-119 체크인 사용시간 데이터 보정
    - 실제로는 이 코드를 사용하지 않고 SQL Query로 보정함

--- src/controllers/user.check.ts
+++ src/controllers/user.check.ts
@@ -5,7 +5,7 @@ import httpStatus from 'http-status';
 import logger from '@modules/logger';
 import ApiError from "@modules/api.error";
 import {getConfig} from "@service/config.service";
-import {getTimeNumber, getTimezoneDateString} from "@modules/util";
+import {getLocalDate, getTimeNumber, getTimezoneDate} from "@modules/util";
 import {getUser} from "@service/user.service";
 
 const isBetween = (target: string, min: any, max: any) => {
@@ -16,14 +16,14 @@ const isBetween = (target: string, min: any, max: any) => {
     return (now >= checkin_at) && (now < checkout_at);
 }
 
-const checkAvailable = async (msg: string, req: Request, res: Response, next: NextFunction) => {
+const isCheckAvailable = async (msg: string, req: Request, res: Response, next: NextFunction) => {
     logger.log(req.user?.jwt, req.params?.cardid);
 
     const user = await getUser(req.user?.jwt?._id);
-    if (['admin'].includes(user.type)) return;
+    if (['admin'].includes(user.type)) return true;
 
     // noinspection DuplicatedCode
-    const today = getTimezoneDateString(new Date()).slice(0, 10)
+    const today = getTimezoneDate(new Date()).toISOString().slice(0, 10)
     const config = await getConfig(today);
     if (!config) {
         let msg = `해당 날짜(${today})의 설정값이 서버에 존재하지 않습니다.`;
@@ -31,15 +31,19 @@ const checkAvailable = async (msg: string, req: Request, res: Response, next: Ne
         throw new ApiError(httpStatus.INTERNAL_SERVER_ERROR, msg, {stack: new Error(msg).stack});
     }
 
-    let now = (new Date().toISOString()).slice(11, 19);
+    let now = getTimezoneDate(new Date()).toISOString().slice(11, 19);
     if (!isBetween(now, config.checkin_at, config.checkout_at)) {
         logger.log('now: ', now, ', checkin_at: ', config.checkin_at, ', checkout_at: ', config.checkout_at);
         let msg = '체크아웃 가능 시간이 아닙니다.';
-        errorHandler(new ApiError(httpStatus.NOT_ACCEPTABLE, msg, {
+        errorHandler(new ApiError(httpStatus.NOT_FOUND, msg, {
             stack: new Error(msg).stack,
-            isFatal: true
+            isFatal: false
         }), req, res, next);
+
+        return false;
     }
+
+    return true;
 }
 
 /**
@@ -49,7 +53,7 @@ export const checkIn = async (req: Request, res: Response, next: NextFunction) =
     try {
         logger.log(req.user?.jwt, req.params?.cardid);
 
-        await checkAvailable('체크인 가능 시간이 아닙니다.', req, res, next);
+        if (!await isCheckAvailable('체크인 가능 시간이 아닙니다.', req, res, next)) return;
         const body = await userService.checkIn(req.user.jwt, req.params.cardid);
         logger.info(body);
         logger.res(httpStatus.OK, body);
@@ -67,7 +71,7 @@ export const checkOut = async (req: Request, res: Response, next: NextFunction) 
     try {
         logger.log(req.user?.jwt);
 
-        await checkAvailable('체크아웃 가능 시간이 아닙니다.', req, res, next);
+        if (!await isCheckAvailable('체크아웃 가능 시간이 아닙니다.', req, res, next)) return;
         const body = await userService.checkOut(req.user.jwt);
         logger.info(body);
         logger.res(httpStatus.OK, body);
