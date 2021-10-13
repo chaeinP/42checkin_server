9417b02145a9b606c237d8eb7e19c6640f5feefe
Jongin Oh <ohjongin@gmail.com>
2021-10-14 00:37:50 +0900

    CHKN-120 쿼리 변경 후 프론트엔드에 맞춰서 컬럼 이름 alias 적용

--- src/service/history.service.ts
+++ src/service/history.service.ts
@@ -1,18 +1,18 @@
-import { clusterCondition, CLUSTER_CODE } from '@modules/cluster';
+import {CLUSTER_CODE, clusterCondition} from '@modules/cluster';
 import logger from '../modules/logger';
 import ApiError from '@modules/api.error';
 import httpStatus from 'http-status';
-import Sequelize, { Op } from 'sequelize';
-import { Users } from 'src/models/users';
-import { History } from '@models/history';
-import { now } from '@modules/util';
+import Sequelize, {Op} from 'sequelize';
+import {Users} from 'src/models/users';
+import {History} from '@models/history';
+import {now} from '@modules/util';
 
 /**
  * 유저의 로그정보를 조회한다.
  */
 export const getUserHistory = async (login: string, page: number, listSize: number) => {
     logger.log('user:', login);
-	const { rows, count } = await History.findAndCountAll({
+    const {rows, count} = await History.findAndCountAll({
         include: [{
             model: Users,
             attributes: ['state', '_id', 'login', 'card_no'],
@@ -23,11 +23,11 @@ export const getUserHistory = async (login: string, page: number, listSize: numb
                 Sequelize.literal('`User`.`login` = `History`.`login`'),
             ],
         },
-		order: [ [ '_id', 'DESC' ] ],
-		offset: listSize * (page - 1),
-		limit: listSize,
-	});
-	return { list: rows, lastPage: Math.ceil(count / listSize) };
+        order: [['_id', 'DESC']],
+        offset: listSize * (page - 1),
+        limit: listSize,
+    });
+    return {list: rows, lastPage: Math.ceil(count / listSize)};
 };
 
 /**
@@ -35,7 +35,7 @@ export const getUserHistory = async (login: string, page: number, listSize: numb
  */
 export const getCardHistory = async (id: number, page: number, listSize: number) => {
     logger.log('card_no:', id);
-	const { rows, count } = await History.findAndCountAll({
+    const {rows, count} = await History.findAndCountAll({
         include: [{
             model: Users,
             attributes: ['state', '_id', 'login', 'card_no'],
@@ -46,11 +46,11 @@ export const getCardHistory = async (id: number, page: number, listSize: number)
                 Sequelize.literal('`User`.`login` = `History`.`login`'),
             ],
         },
-		order: [ [ '_id', 'DESC' ] ],
-		offset: listSize * (page - 1),
-		limit: listSize,
-	});
-	return { list: rows, lastPage: Math.ceil(count / listSize) };
+        order: [['_id', 'DESC']],
+        offset: listSize * (page - 1),
+        limit: listSize,
+    });
+    return {list: rows, lastPage: Math.ceil(count / listSize)};
 };
 
 /**
@@ -58,30 +58,30 @@ export const getCardHistory = async (id: number, page: number, listSize: number)
  */
 export const create = async (user: Users, type: string): Promise<void> => {
     logger.log('user:', JSON.stringify(user));
-	const log = await History.create({
+    const log = await History.create({
         login: user.login,
         card_no: user.card_no,
         type,
         created_at: now().toDate()
     });
 
-	await log.save();
+    await log.save();
 };
 
 /**
  * 클러스터별 로그정보를 조회한다.
  */
 export const getCluster = async (clusterType: CLUSTER_CODE, page: number, listSize: number) => {
-	if (!CLUSTER_CODE[clusterType]) {
+    if (!CLUSTER_CODE[clusterType]) {
         let msg = `존재하지 않는 클러스터 코드(${clusterType})입니다.`;
         throw new ApiError(httpStatus.NOT_FOUND, msg, {stack: new Error(msg).stack});
     }
     logger.log('clusterType:', clusterType, 'cluster:', CLUSTER_CODE[clusterType], 'page:', page, 'listSize:', listSize);
 
-    page = isNaN(page) ? 1: page;
-    listSize = isNaN(listSize) ? 50: listSize;
+    page = isNaN(page) ? 1 : page;
+    listSize = isNaN(listSize) ? 50 : listSize;
 
-    const { rows, count } = await History.findAndCountAll({
+    const {rows, count} = await History.findAndCountAll({
         include: [{
             model: Users,
             attributes: ['state', '_id', 'login', 'card_no'],
@@ -92,13 +92,13 @@ export const getCluster = async (clusterType: CLUSTER_CODE, page: number, listSi
                 Sequelize.literal('`User`.`login` = `History`.`login`'),
             ],
         },
-        order: [ [ '_id', 'DESC' ] ],
+        order: [['_id', 'DESC']],
         offset: listSize * (page - 1),
         limit: listSize,
     });
 
     logger.log(JSON.stringify(rows), count, listSize);
-	return { list: rows, lastPage: Math.ceil(count / listSize) };
+    return {list: rows, lastPage: Math.ceil(count / listSize)};
 };
 
 /**
@@ -111,18 +111,19 @@ export const getCheckIn = async (clusterType: CLUSTER_CODE, page: number, listSi
     }
     logger.log('clusterType:', clusterType, ',cluster:', CLUSTER_CODE[clusterType], ',page:', page, ',listSize:', listSize);
 
-    page = isNaN(page) ? 1: page;
-    listSize = isNaN(listSize) ? 50: listSize;
+    page = isNaN(page) ? 1 : page;
+    listSize = isNaN(listSize) ? 50 : listSize;
 
-	const { rows, count } = await Users.findAndCountAll({
-		where: Sequelize.and(
-            { card_no: clusterCondition[clusterType] },
-            { card_no: { [Op.ne]: null } }),
-		order: [ [ '_id', 'DESC' ] ],
-		offset: listSize * (page - 1),
-		limit: listSize,
-	});
+    const {rows, count} = await Users.findAndCountAll({
+        attributes: ['_id', ['checkin_at', 'created_at'], 'state', 'login', 'card_no'],
+        where: Sequelize.and(
+            {card_no: clusterCondition[clusterType]},
+            {card_no: {[Op.ne]: null}}),
+        order: [['_id', 'DESC']],
+        offset: listSize * (page - 1),
+        limit: listSize,
+    });
 
-	return { list: rows, lastPage: Math.ceil(count / listSize) };
+    return {list: rows, lastPage: Math.ceil(count / listSize)};
 };
 
